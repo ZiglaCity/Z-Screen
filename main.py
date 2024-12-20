995f7b32f192348e8e0524b2c6123a58d3b02cbb
ZiglaCity <ziglacity@gmail.com>
2024-12-20 16:45:14 +0000

    App window set

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+class ScreenApp:
+    def __init__(self, root):
+        pass
+
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ScreenApp(root)
+    root.mainloop()

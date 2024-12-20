d8b565da26ffac1b88de2c773a3276c6ee69522c
ZiglaCity <ziglacity@gmail.com>
2024-12-20 17:06:21 +0000

    UI of application set with declared functionalities

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import os
+from tkinter import ttk
 
 
 class ScreenApp:
@@ -17,6 +18,43 @@ class ScreenApp:
         
         if not os.path.exists(self.save_location.get()):
             os.makedirs(self.save_location.get())
+
+        
+        ttk.Label(root, text="Max Recording Time (seconds):").pack(pady=10)
+        
+        self.combo = ttk.Combobox(root, values=list(range(5, 61, 5)), textvariable=self.max_time, state="readonly")
+        self.combo.pack(pady=5)
+        
+        ttk.Label(root, text="Save Location:").pack(pady=10)
+        self.save_location_entry = ttk.Entry(root, textvariable=self.save_location, width=40)
+        self.save_location_entry.pack(pady=5)
+        ttk.Button(root, text="Browse", command=self.browse_save_location).pack(pady=5)
+        
+        self.start_button = ttk.Button(root, text="Start Recording", command=self.start_recording)
+        self.start_button.pack(pady=5)
+        
+        self.stop_button = ttk.Button(root, text="Stop Recording", command=self.stop_recording, state="disabled")
+        self.stop_button.pack(pady=5)
+        
+        self.screenshot_button = ttk.Button(root, text="Take Screenshot", command=self.take_screenshot)
+        self.screenshot_button.pack(pady=5)
+
+    
+    def browse_save_location(self):
+        pass
+
+    def take_screenshot(self):
+        pass
+
+    def capture_screenshot():
+        pass
+
+    def stop_recording(self):
+        pass
+
+    def start_recording(self):
+        pass
+
 
 if __name__ == "__main__":
     root = tk.Tk()
